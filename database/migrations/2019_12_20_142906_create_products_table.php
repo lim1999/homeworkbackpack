6c2b7b6ec3179bbc9dd10f14f207518a9c1159ad
unknown <rean.it123@gmail.com>
2019-12-20 21:36:29 +0700

    create table category and product

---
+++ database/migrations/2019_12_20_142906_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->tinyinteger('category_id')->nullable();
+            $table->string('name')->nullable();
+            $table->float('list_price')->nullable();
+            $table->float('sale_price')->nullable();
+            $table->float('sold_price')->nullable();
+            $table->float('rent_price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
